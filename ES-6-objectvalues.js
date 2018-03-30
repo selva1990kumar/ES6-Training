a574545f46a39db3a56b1816f4a2c301ead45294
Selvakumar T <selvakumar_tha@photonint.com>
2018-03-30 11:48:27 +0530

    ShortHandPropertyES6

---
+++ ES-6-objectvalues.js
@@ -0,0 +1,11 @@
+//Short Hand Property in ES6
+//ES6
+let selva = "firstValue";
+let kumar = "secondValue";
+
+let firstObject = { selva, kumar };
+console.log(firstObject);
+
+//ES5
+let secondObject = { selva: "firstValue", kumar: "secondValue" };
+console.log(secondObject);
